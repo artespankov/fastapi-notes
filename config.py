43d25daa401df15f9c50c3dbbf533ef33cd16892
artempankov <artespankov@gmail.com>
2020-11-26 23:29:04 +0200

    idea > ignore

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ db_host = os.environ.get('db_host', 'localhost')
 db_port = str(os.environ.get('db_port', '5432'))
 db_name = str(os.environ.get('db_name', 'fastapi-notes'))
 db_username = str(os.environ.get('db_username', 'postgres'))
-db_password = str(os.environ.get('db_password', 'qwerty'))
+db_password = str(os.environ.get('db_password', ''))
 ssl_mode = str(os.environ.get('ssl_mode', 'prefer'))
 
 
